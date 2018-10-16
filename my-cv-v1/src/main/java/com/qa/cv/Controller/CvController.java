83e89984822f2111418ea5a383948787d32d7937
Xavhyatt <42604097+Xavhyatt@users.noreply.github.com>
2018-10-16 19:02:43 +0100

    Add files via upload

--- my-cv-v1/src/main/java/com/qa/cv/Controller/CvController.java
+++ my-cv-v1/src/main/java/com/qa/cv/Controller/CvController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.cv.Exceptions.ResourceNotFoundException;
 import com.qa.cv.Model.CvModel;
-import com.qa.cv.Model.DepartmentModel;
+import com.qa.cv.Model.UsersDataModel;
 import com.qa.cv.Repositories.CvRepository;
+import com.qa.cv.Repositories.DepartmentRepository;
+import com.qa.cv.Repositories.UserRepository;
 
 @RestController
 @RequestMapping("api/")
@@ -25,10 +29,22 @@ public class CvController {
 	@Autowired
 	CvRepository cvRepository;
 	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	DepartmentRepository departmentRepository;
+	
+	
 	//Method to Post a Cv
-	@PostMapping("/cv")
-	public CvModel createCv(@Valid @RequestBody CvModel mSDM) {
-		return cvRepository.save(mSDM);
+	@PostMapping("/department/{departmentId}/user/{userId}/cv")
+	public CvModel addCv(@PathVariable(value = "departmentId") Long departmentId,
+			@PathVariable(value = "userId") Long userId, @Valid @RequestBody CvModel cvModel, UsersDataModel usersDataModel) {
+			return userRepository.findById(userId).map(userModel -> {
+				cvModel.setUserId(userModel);
+				return cvRepository.save(cvModel);
+			}).orElseThrow(() -> new ResourceNotFoundException("User", "id", usersDataModel));
+	
 	}
 	
 	//Method to Get a Cv
@@ -43,18 +59,39 @@ public class CvController {
 		return cvRepository.findAll();
 	}
 	
-	//Method to Edit a Cv
-	@PutMapping("/cv/{id}")
-	public CvModel updateCv(@PathVariable(value = "id") Long cvID,
-			@Valid @RequestBody CvModel cvDetails) {
+	// Method to Edit a Cv
+	@PutMapping("/department/{departmentId}/user/{userId}/cv/{cvId}")
+	public CvModel updateUser(@PathVariable(value = "userId") Long userId,
+			@PathVariable(value = "cvId") Long cvId,
+			@Valid @RequestBody CvModel cvRequest) {
 		
-		CvModel mSDM = cvRepository.findById(cvID).orElseThrow(() -> new ResourceNotFoundException("Cv", "id", cvID));
-			
-		mSDM.setCvLink(cvDetails.getCvLink());
-		mSDM.setStatus(cvDetails.getStatus());
-			
-		CvModel updateData = cvRepository.save(mSDM);
-		return updateData;
+		if(!userRepository.existsById(userId)) {
+			throw new ResourceNotFoundException("Department", "id", cvRequest);
+		}
+		
+		return cvRepository.findById(cvId).map( cv -> {
+			cv.setCvLink(cvRequest.getCvLink());
+			cv.setStatus(cvRequest.getStatus());
+			return cvRepository.save(cv);
+		}).orElseThrow(() -> new ResourceNotFoundException("CV", "id", cvRequest));
 	}
+
+	// Method to remove a user
+		@DeleteMapping("/department/{departmentId}/user/{userId}/cv/{cvId}")
+		public ResponseEntity<?> deleteCv(@PathVariable(value = "userId") Long userId,
+				@PathVariable(value = "cvId") Long cvId) {
+			
+			if(!userRepository.existsById(userId)) {
+				throw new ResourceNotFoundException("Department", "id", userId);
+			}
+			
+			return cvRepository.findById(cvId).map(cv -> {
+				cvRepository.delete(cv);
+				return ResponseEntity.ok().build();
+						}).orElseThrow(() -> new ResourceNotFoundException("UserId", userId.toString(), null));
+
+		
+
+		}
 	
 }
--- my-cv-v1/src/main/java/com/qa/cv/Repositories/UserRepository.java
+++ my-cv-v1/src/main/java/com/qa/cv/Repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.qa.cv.Repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.qa.cv.Model.UsersDataModel;
 @Repository
 public interface UserRepository extends JpaRepository <UsersDataModel,Long>{
 
+	Page<UsersDataModel> findByDepartmentId(Long departmentId, Pageable pageable);
 	
 
 }
