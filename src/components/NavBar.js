7d4a9fc3d54789dafa2d411b400374e563a05b68
vinuruban <vinu_06@hotmail.com>
2018-10-21 20:29:18 +0100

    navigation bar completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Webpages/Home";
+import Login from "./Webpages/Login";
+import CVDashboard from "./Webpages/CVDashboard";
 
 class NavBar extends Component {
   state = {};
   render() {
-    return <div>NavBar</div>;
+    return (
+      <HashRouter>
+        <div>
+          <ul className="header">
+            <li>
+              <NavLink exact to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cvdashboard">CV Dashboard</NavLink>
+            </li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/cvdashboard" component={CVDashboard} />
+          </div>
+        </div>
+      </HashRouter>
+    );
   }
 }
 
