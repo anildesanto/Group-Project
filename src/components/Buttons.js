770f9846180314d5713ffb02c9be59ddfb6414b9
Mitch Heath <mitchheath@me.com>
2018-10-22 18:31:27 +0100

    CVRow

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,40 +1,70 @@
 import React, { Component } from "react";
-
+import axios from 'axios';
 class Buttons extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      upload: props.upload
+      cvModel: 
+      {
+  "cvId": 3,
+  "cvLink": {
+    "binaryStream": {
+      
+    }
+  },
+  "fileType": "application\/pdf",
+  "fileName": "QAC_Consultant_Learning_Platform_Brochure.pdf",
+  "creationDate": "2018-10-21T17:47:04.200+0000",
+  "status": "Gray",
+  "user": {
+    "userId": 1,
+    "firstName": "Aizen",
+    "lastName": "Hisoka",
+    "email": "anime",
+    "password": "lives",
+    "handler": {
+      
+    },
+    "department": {
+      "departmentId": 2,
+      "role": "Trainer",
+      "handler": {
+        
+      },
+      "hibernateLazyInitializer": {
+        
+      }
+    },
+    "hibernateLazyInitializer": {
+      
+    }
+  }
+}
     };
   }
-
-  // static defaultProps = {
-  //   userID: "1",
-  //   deptID: "1"
-  // };
-
   render() {
     return (
       <div>
+        <p>{this.state.cvModel + " test"}</p>
+        {/* <p>{"userI" + this.state.cvModel.user.userId}</p> */}
+        <p>{"userI" + this.state.cvModel.user.userId}</p>
         <form
           action={
             "https://qacvmanager.azurewebsites.net/api/user/" +
-            this.props.userID +
-            "/upload"
+            this.state.cvModel.user.userId + "/upload"
           }
           method="post"
           header="file"
           enctype="multipart/form-data"
         >
           <input type="file" name="file" />
-
           <input type="submit" value="Upload" />
         </form>
         <a
           href={
             "https://qacvmanager.azurewebsites.net/api/cv/" +
-            this.props.cvID +
-            "/download"
+            this.state.cvModel.cvId + "/download"
           }
         >
           Download
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,26 @@ import Buttons from "./Buttons";
 import InfoBar from "./InfoBar";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
+import CVRow from "./CVRow";
 
 class Main extends Component {
-  state = {
-    userId: "1",
-    cvId: "12"
+  static defaultProps = {
+    userID: "1",
+    cvID: "12"
   };
   render() {
     return (
       <div>
         <h3>Buttons Component:</h3>
         {/* <Buttons upload={(userId = "1")} /> */}
-        <Buttons userId={this.props.userId} cvId={this.props.cvId} />
+        <Buttons cvID={this.props.cvID} />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h3> CVRow Component:</h3>
+        <CVRow />
         <br />
         <br />
         <br />
